c0681201918c5261223a15aa5da32f57d7e3c530
cgalvan01kamey <cracklitosprojectv0@protonmail.com>
2026-06-27 11:34:43 -0600

    project created and gitignore file added

---
+++ iExpenseV2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iExpenseV2
+//
+//  Created by Jose Carlos Galvan Kamey on 6/27/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iExpenseV2/iExpenseV2App.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseV2App.swift
+//  iExpenseV2
+//
+//  Created by Jose Carlos Galvan Kamey on 6/27/26.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
